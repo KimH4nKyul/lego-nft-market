15547644d5caa2bf633087199daa365cb73f697f
KimH4nKyul <alskim93@kakao.com>
2022-02-09 11:04:24 +0900

    merge contract

--- contracts/SaleLegoCadDeal.sol
+++ contracts/SaleLegoCadDeal.sol
@@ -6,19 +6,13 @@ import "./LegoCadDeal.sol";
 contract SaleLegoCadDeal {
     LegoCadDeal public legoCadDealAddress;
 
-<<<<<<< HEAD
     mapping(uint256 => bool) onSaleToken;
     mapping(uint256 => uint256) public tokenPrice;
-=======
-    mapping(uint256 => bool) onSale;
-    mapping(uint256 => uint256) price;
->>>>>>> a006635aa343fac8f4d3929a2707d9f54ac2de97
 
     constructor(address _legoCadDealAddress) {
         legoCadDealAddress = LegoCadDeal(_legoCadDealAddress);
     }
 
-<<<<<<< HEAD
     function setTokenPrice(uint256 _tokenId, uint256 _price) public {
         require(_price > 0, "1");
         require(tokenPrice[_tokenId] == 0, "2");
@@ -57,31 +51,5 @@ contract SaleLegoCadDeal {
 
         onSaleToken[_tokenId] = false;
         tokenPrice[_tokenId] = 0;
-=======
-    // 토큰 가격 책정
-    function setPrice(uint256 _tokenId, uint256 _price) public {
-        require(_price > 0, "");
-        address tokenOwner = legoCadDealAddress.ownerOf(_tokenId);
-        require(msg.sender == tokenOwner, "");
-        price[_tokenId] = _price;
-        onSale[_tokenId] = true;
-    }
-
-    // 토큰 구매
-    function purchase(uint256 _tokenId) public payable {
-        require(price[_tokenId] == msg.value, "");
-        address tokenOwner = legoCadDealAddress.ownerOf(_tokenId);
-        require(msg.sender != tokenOwner, "");
-        require(
-            legoCadDealAddress.isApprovedForAll(tokenOwner, msg.sender),
-            ""
-        );
-
-        payable(tokenOwner).transfer(msg.value);
-        legoCadDealAddress.safeTransferFrom(tokenOwner, msg.sender, _tokenId);
-
-        price[_tokenId] = 0;
-        onSale[_tokenId] = false;
->>>>>>> a006635aa343fac8f4d3929a2707d9f54ac2de97
     }
 }
